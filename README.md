6d332404a1efad62166f0c7dbc27e165af5c9248
elias <elias.nehme1@cevt.se>
2022-04-13 14:57:37 +0200

    Implemented download script and added instructions in README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # art-gan-project
+
+## Downloading data
+
+Two kaggle datasets are being used. To get the data, follow the steps below
+
+1. Install kaggle: `pip install kaggle`
+2. Download API credentials from your account page on kaggle and place in  `~/.kaggle/`
+3. Join competition at: 'https://www.kaggle.com/competitions/gan-getting-started/overview'
+4. Run the downloading script: `bash download_data.sh`
+
---
+++ download_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set working directory to root
+cd "$(dirname "$0")"
+
+# Crate data directory if it does not already exist
+mkdir -p data
+
+kaggle datasets download jessicali9530/celeba-dataset -p data/
+unzip data/celeba-dataset.zip -d data/celeba
+rm data/celeba-dataset.zip
+
+kaggle competitions download -c gan-getting-started -p data/
+unzip data/gan-getting-started.zip -d data/art_dataset
+rm data/gan-getting-started.zip
