09e23b6bf0d18535bdba3ca4a65c0528a54c4dde
elias <elias.nehme1@cevt.se>
2022-04-19 16:01:17 +0200

    WIP: Refactor

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-from art_data_module import ArtDataModule, BATCH_SIZE, IMAGE_SIZE
-from celeba_datamodule import CelebADataModule
+from data_modules import data_modules
+import os
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import Trainer
-from cnn_model import DCGAN
+from DCGAN import DCGAN
+
+
+# Dataset
+dataset = "celeba"  # or "art"
 
 # Number of channels in the training images. For color images this is 3
-nc = 3
+num_channels = 3
 
 # Size of z latent vector (i.e. size of generator input)
-nz = 128
+latent_dim = 128
 
 # Size of feature maps in generator
-ngf = 64
+num_generator_features = 64
 
 # Size of feature maps in discriminator
-ndf = 64
+num_discriminator_features = 64
+
+NUM_WORKERS = int(os.cpu_count() / 2)
+BATCH_SIZE = 128
+IMAGE_SIZE = (64, 64)
 
 wandb_logger = WandbLogger(project="gan-project")
-dm = CelebADataModule(batch_size=BATCH_SIZE, num_workers=10, image_size=IMAGE_SIZE)
+dm = data_modules[dataset](batch_size=BATCH_SIZE, num_workers=10, image_size=IMAGE_SIZE)
 
-model = DCGAN(IMAGE_SIZE[0], IMAGE_SIZE[1], nc, nz, ngf, ndf)
+model = DCGAN(
+    IMAGE_SIZE[0],
+    IMAGE_SIZE[1],
+    num_channels,
+    latent_dim,
+    num_generator_features,
+    num_discriminator_features,
+)
 
-trainer = Trainer(gpus=1, max_epochs=1000, log_every_n_steps=30, logger=wandb_logger)
+trainer = Trainer(max_epochs=1000, log_every_n_steps=30, logger=wandb_logger)
 trainer.fit(model, dm)
