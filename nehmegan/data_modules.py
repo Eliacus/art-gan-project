da273d0a64d3d27561d8043e3c383ee4ee743623
Elias <eliasneme2@gmail.com>
2022-08-29 19:44:42 +0200

    WIP: FID metric, now working but needs refactor

--- nehmegan/data_modules.py
+++ nehmegan/data_modules.py
@@ -1,12 +1,9 @@
-from pathlib import Path
-from typing import Optional
-
 import pytorch_lightning as pl
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
-from nehmegan import ROOT
+from nehmegan.data_utils import verify_and_get_dataset_paths
 
 
 class GANImageDataModule(pl.LightningDataModule):
@@ -14,19 +11,25 @@ class GANImageDataModule(pl.LightningDataModule):
         self,
         batch_size: int,
         num_workers: int,
-        dataset: str,
+        dataset_name: str,
         image_size: tuple = (64, 64),
     ):
-        """Generic datamodule for image datasets using the pytorchvision ImageFolder class
+        """
+        Generic datamodule for image datasets using the pytorchvision ImageFolder class
 
         Args:
             batch_size (int): Data batch size
             num_workers (int): Number of cpu workers
+            dataset (str): dataset name. This requires there to be a folder named data/dataset which in turn contains
+                           a folder images/ which contains all the dataset images.
             image_size (tuple): Desired image size
         """
         super().__init__()
-        self.dataset = dataset
-        self.data_path = Path.joinpath(ROOT, "data", dataset, "images")
+        self.dataset_name = dataset_name
+        self.dataset_root_path, self.dataset_images_path = verify_and_get_dataset_paths(
+            self.dataset_name
+        )
+
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.image_size = image_size
@@ -41,12 +44,12 @@ class GANImageDataModule(pl.LightningDataModule):
             ]
         )
 
-    def setup(self, stage: Optional[str] = None):
-        self.ds = ImageFolder(self.data_path, self.transforms)
+    def setup(self):
+        self.dataset = ImageFolder(self.dataset_images_path, self.transforms)
 
     def train_dataloader(self):
         return DataLoader(
-            self.ds,
+            self.dataset,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
